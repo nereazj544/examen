1ca488c31b909a1e117924b2828beedb7a199876
Julio Pedro López Bayón <jplbayon@gmail.com>
2024-05-20 19:46:36 +0200

    Actualización

--- src/dam/psp/ex20230315/Peticion.java
+++ src/dam/psp/ex20230315/Peticion.java
@@ -6,13 +6,16 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class Peticion implements Runnable {
 
 	private final Socket socket;
 	private DataOutputStream out;
 	private DataInputStream in;
-	
+
 	public Peticion(Socket socket) throws IOException {
 		this.socket = socket;
 		socket.setSoTimeout(5000);
@@ -27,13 +30,13 @@ public class Peticion implements Runnable {
 			String peticion = in.readUTF();
 			switch (peticion) {
 			case "hash":
-				
+				peticionHash();
 				break;
 			case "cert":
-				
+
 				break;
 			case "cifrar":
-				
+
 				break;
 			default:
 				enviarRespuesta(String.format("ERROR:'%s' no se reconoce como una petición válida", peticion));
@@ -51,7 +54,7 @@ public class Peticion implements Runnable {
 			}
 		}
 	}
-	
+
 	private void enviarRespuesta(String respuesta) {
 		System.out.println(socket.getInetAddress() + " -> " + respuesta);
 		try (socket) {
@@ -59,6 +62,50 @@ public class Peticion implements Runnable {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+//	private void peticionHash() {
+//		try {
+//			String algoritmo = in.readUTF();
+//			MessageDigest md = MessageDigest.getInstance(algoritmo);
+//			byte [] bytes = in.readAllBytes();
+//			if (bytes.length == 0)
+//				enviarRespuesta("ERROR:Se esperaban datos");
+//			else {
+//				byte [] hash = md.digest(bytes);
+//				enviarRespuesta("OK:" + Base64.getEncoder().encodeToString(hash));
+//			}
+//		} catch (SocketTimeoutException e) {
+//			enviarRespuesta("ERROR:Read timed out");
+//		} catch (EOFException | NoSuchAlgorithmException e) {
+//			enviarRespuesta("ERROR:Se esperaba un algoritmo");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} 
+//	}
+	
+	private void peticionHash() {
+		try {
+			String algoritmo = in.readUTF();
+			MessageDigest md = MessageDigest.getInstance(algoritmo);
+			int n;
+			int contador = 0;
+			byte [] bytes = new byte[1024];
+			while ((n = in.read(bytes)) != -1) {
+				contador += n;
+				md.update(bytes, 0, n);
+			}
+			if (contador == 0)
+				enviarRespuesta("ERROR:Se esperaban datos");
+			else
+				enviarRespuesta("OK:" + Base64.getEncoder().encodeToString(md.digest()));
+		} catch (SocketTimeoutException e) {
+			enviarRespuesta("ERROR:Read timed out");
+		} catch (EOFException | NoSuchAlgorithmException e) {
+			enviarRespuesta("ERROR:Se esperaba un algoritmo");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
